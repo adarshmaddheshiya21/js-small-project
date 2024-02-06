308194b68964378aa8a2e4e931259d9e1c8df77a
aadiaditya247 <aadiaditya247@gmail.com>
2024-02-06 23:25:01 +0530

    first commit

---
+++ 02-BMI-Calculator/chaiaurcode.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault() // pravent to post form on server
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const result = document.querySelector('#results')
+
+    if(height === '' || height < 0 || isNaN(height)) {
+        result.innerHTML = `Please provide a valid height`
+    } else if(weight === '', weight < 0, isNaN(weight)) {
+        result.innerHTML = "Please provide a valid weight"
+    } else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2)
+        if(bmi < 18.6) {
+            result.innerHTML = `<span>${bmi}</span>`
+            // result.innerHTML = `<span>Under weight</span>`
+        } else if(bmi >= 18.6 || bmi < 24.9) {
+            result.innerHTML = `<span>${bmi}</span>`
+            // result.innerHTML = `<span>Normal weight</span>`
+        } else {
+            result.innerHTML = `<span>${bmi}</span>`
+            // result.innerHTML = `<span>Over weight</span>`
+        }
+    }
+})
